d5b0d621f09a4d6ebf78ba794aad294c8baacf13
jwatmore <cornflourblue@hotmail.com>
2020-04-19 13:56:13 +1000

    added url check to jwt interceptor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
